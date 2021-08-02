13b301fafdc471b3401901d46d4603848ca7779d
Dilara Özdemir <42623884+dilaraozdemir@users.noreply.github.com>
2021-08-02 17:52:31 +0300

    Update load.js

--- js/load.js
+++ js/load.js
@@ -40,26 +40,3 @@ function loadResume() {
 loadSocialIcons()
 loadResume()
 
-function dayNightToggle() {
-    var element = document.body;
-    element.classList.toggle("light-mode");
-    var check = window.localStorage.getItem('light-mode');
-    if (check == "light") {
-        window.localStorage.clear();
-    } else {
-        window.localStorage.setItem('light-mode', 'light');
-    }
-
-}
-
-function checkDayNight() {
-    var check = window.localStorage.getItem('light-mode');
-    var element = document.body;
-    if (check == "light") {
-        element.classList.add("light-mode");
-    } else {
-        element.classList.remove("light-mode");
-    }
-}
-
-checkDayNight()
