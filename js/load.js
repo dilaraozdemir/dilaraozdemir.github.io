5d0a9e2862a2120974650525a2c02d9a400100eb
Dilara Özdemir <42623884+dilaraozdemir@users.noreply.github.com>
2021-08-02 20:17:23 +0300

    Update load.js

--- js/load.js
+++ js/load.js
@@ -40,28 +40,5 @@ function loadResume() {
 loadResume()
 
 
-function dayNightToggle() {
-    var element = document.body;
-    element.classList.toggle("light-mode");
-    var check = window.localStorage.getItem('light-mode');
-    if (check == "light") {
-        window.localStorage.clear();
-    } else {
-        window.localStorage.setItem('light-mode', 'light');
-    }
-
-}
-
-function checkDayNight() {
-    var check = window.localStorage.getItem('light-mode');
-    var element = document.body;
-    if (check == "light") {
-        element.classList.add("light-mode");
-    } else {
-        element.classList.remove("light-mode");
-    }
-}
-
-checkDayNight()
 
 
