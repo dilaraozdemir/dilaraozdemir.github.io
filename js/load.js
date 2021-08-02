2bca69f91a5ede1c54d3f1dc585ffef34865523c
Dilara Özdemir <42623884+dilaraozdemir@users.noreply.github.com>
2021-08-02 17:52:49 +0300

    Update load.js

--- js/load.js
+++ js/load.js
@@ -36,6 +36,29 @@ function loadResume() {
     }
     document.getElementById("resume").innerHTML = socialtext;
 }
+function dayNightToggle() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    var check = window.localStorage.getItem('light-mode');
+    if (check == "light") {
+        window.localStorage.clear();
+    } else {
+        window.localStorage.setItem('light-mode', 'light');
+    }
+
+}
+
+function checkDayNight() {
+    var check = window.localStorage.getItem('light-mode');
+    var element = document.body;
+    if (check == "light") {
+        element.classList.add("light-mode");
+    } else {
+        element.classList.remove("light-mode");
+    }
+}
+
+checkDayNight()
 
 loadSocialIcons()
 loadResume()
